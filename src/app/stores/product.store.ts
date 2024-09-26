1af96eac98b08f4e4d0aff48fbd44f1d1771857f
hktari <bkamnik1995@gmail.com>
2024-09-26 22:31:06 +0200

    use environment variables for api endpoint

--- src/app/stores/product.store.ts
+++ src/app/stores/product.store.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { signal } from '@angular/core';
 import { Product, ProductId } from '../models/product.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,23 +12,27 @@ export class ProductStore {
 
   constructor(private http: HttpClient) {}
 
+  _appendToBaseURL(url: string) {
+    return new URL(url, environment.apiUrl).toString();
+  }
+
   getProducts() {
     return this.products();
   }
 
   fetchSingleProduct(id: ProductId) {
-    return this.http.get<Product>('http://localhost:3000/products/' + id);
+    return this.http.get<Product>(this._appendToBaseURL(`/products/${id}`));
   }
 
   fetchProducts() {
     this.http
-      .get<Product[]>('http://localhost:3000/products')
+      .get<Product[]>(this._appendToBaseURL('/products'))
       .subscribe((data) => this.products.set(data));
   }
 
   addProduct(product: Product) {
     this.http
-      .post<Product>('http://localhost:3000/products', product)
+      .post<Product>(this._appendToBaseURL('/products'), product)
       .subscribe((newProduct) =>
         this.products.update((products) => [...products, newProduct])
       );
@@ -35,7 +40,7 @@ export class ProductStore {
 
   updateProduct(product: Product) {
     this.http
-      .put<Product>(`http://localhost:3000/products/${product.id}`, product)
+      .put<Product>(this._appendToBaseURL(`products/${product.id}`), product)
       .subscribe((updatedProduct) =>
         this.products.update((products) =>
           products.map((p) => (p.id === updatedProduct.id ? updatedProduct : p))
@@ -45,7 +50,7 @@ export class ProductStore {
 
   deleteProduct(id: ProductId) {
     this.http
-      .delete(`http://localhost:3000/products/${id}`)
+      .delete(this._appendToBaseURL(`products/${id}`))
       .subscribe(() =>
         this.products.update((products) => products.filter((p) => p.id !== id))
       );
