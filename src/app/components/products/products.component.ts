f8c62978f5429abcdbe23cac3d6d9676dcae7625
hktari <bkamnik1995@gmail.com>
2024-09-26 22:17:19 +0200

    edit heading size

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,8 +10,8 @@ import { ProductListItemComponent } from '../product-list-item/product-list-item
   imports: [RouterLink, ProductListItemComponent],
   selector: 'app-products',
   template: `
-    <div class="container mx-auto p-4">
-      <h1 class="text-2xl font-bold mb-4">Products</h1>
+    <div class="container mx-auto p-10">
+      <h1 class="text-4xl font-bold mb-4">Products</h1>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div class="p-4">
           @for (product of productStore.getProducts(); track product.id) {
